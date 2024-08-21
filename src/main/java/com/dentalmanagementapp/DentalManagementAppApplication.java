97098bff53193fae492f02ab3c1f8f1b276377e6
Kamen <genchev.kamen@gmail.com>
2024-08-21 11:05:02 +0300

    Initial commit

---
+++ src/main/java/com/dentalmanagementapp/DentalManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.dentalmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentalmanagementapp/DentalManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

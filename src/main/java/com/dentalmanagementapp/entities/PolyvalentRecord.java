949968d05dbd35163d7d88ed3f0beee4b54dcbb3
Kamen <genchev.kamen@gmail.com>
2024-08-22 15:06:23 +0300

    Add PolyvalentRecord entity

---
+++ src/main/java/com/dentalmanagementapp/entities/PolyvalentRecord.java
@@ -0,0 +1,9 @@
+package com.dentalmanagementapp.entities;
+
+import com.dentalmanagementapp.entities.common.PatientRecord;
+import jakarta.persistence.Entity;
+
+@Entity
+public class PolyvalentRecord extends PatientRecord {
+
+}

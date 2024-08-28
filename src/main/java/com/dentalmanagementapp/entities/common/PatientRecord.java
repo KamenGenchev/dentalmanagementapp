dbef6e624e076bd3d94030812d7f9c8dfd11cd72
Kamen <genchev.kamen@gmail.com>
2024-08-28 12:55:16 +0300

    Refactored Patient with DentistPatient

--- src/main/java/com/dentalmanagementapp/entities/common/PatientRecord.java
+++ src/main/java/com/dentalmanagementapp/entities/common/PatientRecord.java
@@ -1,19 +1,44 @@
 package com.dentalmanagementapp.entities.common;
 
-import com.dentalmanagementapp.entities.Patient;
+import com.dentalmanagementapp.entities.DentistPatient;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 @MappedSuperclass
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class PatientRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
-    private Patient patient;
+    private DentistPatient dentistPatient;
     private String description;
     private LocalDate recordDate;
+
+    protected PatientRecord() {
+    }
+
+    protected PatientRecord(@NotNull String description, @NotNull LocalDate recordDate, @NotNull DentistPatient dentistPatient) {
+        this.description = description;
+        this.recordDate = recordDate;
+        this.dentistPatient = dentistPatient;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public DentistPatient getDentistPatient() {
+        return dentistPatient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getRecordDate() {
+        return recordDate;
+    }
 
 }

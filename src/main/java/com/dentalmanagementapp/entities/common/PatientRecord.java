b6fcba073a887012053b77fa3ae027743ee69b70
Kamen <genchev.kamen@gmail.com>
2024-09-09 11:33:44 +0300

    Add soft delete functionality to PatientRecord

--- src/main/java/com/dentalmanagementapp/entities/common/PatientRecord.java
+++ src/main/java/com/dentalmanagementapp/entities/common/PatientRecord.java
@@ -3,26 +3,31 @@ package com.dentalmanagementapp.entities.common;
 import com.dentalmanagementapp.entities.DentistPatient;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.SoftDelete;
 
 import java.time.LocalDate;
 
 @MappedSuperclass
+@SoftDelete
 public abstract class PatientRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private boolean deleted;
     @ManyToOne(fetch = FetchType.LAZY)
     private DentistPatient dentistPatient;
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false)
     private LocalDate recordDate;
 
     protected PatientRecord() {
     }
 
-    protected PatientRecord(@NotNull String description, @NotNull LocalDate recordDate, @NotNull DentistPatient dentistPatient) {
+    protected PatientRecord(String description, DentistPatient dentistPatient) {
         this.description = description;
-        this.recordDate = recordDate;
         this.dentistPatient = dentistPatient;
+        this.recordDate = LocalDate.now();
     }
 
     public Long getId() {

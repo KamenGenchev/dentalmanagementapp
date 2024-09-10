d57f73641435277b190fc5412457851ec506b708
Kamen <genchev.kamen@gmail.com>
2024-09-10 13:22:35 +0300

    Refactor DentistServiceImpl and improve validation logic

--- src/main/java/com/dentalmanagementapp/service/implementation/DentistServiceImpl.java
+++ src/main/java/com/dentalmanagementapp/service/implementation/DentistServiceImpl.java
@@ -1,68 +1,96 @@
 package com.dentalmanagementapp.service.implementation;
 
-import com.dentalmanagementapp.dtos.DentistRegisterDto;
+import com.dentalmanagementapp.dtos.dentist.DentistDetailsDto;
+import com.dentalmanagementapp.dtos.dentist.DentistDto;
+import com.dentalmanagementapp.dtos.dentist.DentistRegisterDto;
 import com.dentalmanagementapp.entities.Dentist;
-import com.dentalmanagementapp.exception.custom.EntityAlreadyExistsException;
+import com.dentalmanagementapp.exception.custom.NotFoundException;
+import com.dentalmanagementapp.mappers.DentistMapper;
 import com.dentalmanagementapp.repository.DentistRepository;
 import com.dentalmanagementapp.service.DentistService;
+import com.dentalmanagementapp.validation.DentistValidation;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @Service
+@Validated
 public class DentistServiceImpl implements DentistService {
     private final DentistRepository dentistRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final DentistMapper dentistMapper;
+    private final DentistValidation dentistValidation;
 
     @Autowired
-    public DentistServiceImpl(DentistRepository dentistRepository, PasswordEncoder passwordEncoder) {
+    public DentistServiceImpl(DentistRepository dentistRepository, DentistMapper dentistMapper, DentistValidation dentistValidation) {
         this.dentistRepository = dentistRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.dentistMapper = dentistMapper;
+        this.dentistValidation = dentistValidation;
     }
 
     @Override
-    public void registerDentist(DentistRegisterDto dto) {
-        if (dentistRepository.findByEmail(dto.email()).isPresent()) {
-            throw new EntityAlreadyExistsException("Dentist with this email already exists");
-        }
+    @Transactional
+    public void registerDentist(@Valid DentistRegisterDto dto) {
+        dentistValidation.assertDoesNotExistByEmail(dto.email());
+        Dentist dentist = dentistMapper.fromRegisteringDto(dto);
 
-        Dentist dentist = new Dentist();
-        dentist.setFirstName(dto.firstName());
-        dentist.setLastName(dto.lastName());
-        dentist.setEmail(dto.email());
-        dentist.setPassword(passwordEncoder.encode(dto.password()));
         dentistRepository.save(dentist);
     }
 
     @Override
-    public Dentist createDentist(Dentist dentist) {
-        return null;
+    @Transactional
+    public Long createDentist(@Valid DentistDto dto) {
+        dentistValidation.assertDoesNotExistByEmail(dto.email());
+        Dentist dentist = dentistMapper.fromAdminDto(dto);
+
+        dentistRepository.save(dentist);
+        return dentist.getId();
     }
 
     @Override
-    public Optional<Dentist> getDentistById(Long id) {
-        return dentistRepository.findById(id);
+    public List<DentistDto> getAllDentists() {
+        return dentistRepository.findAll().stream()
+                .map(dentistMapper::toDto)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), List::copyOf));
     }
 
     @Override
-    public List<Dentist> getAllDentists() {
-        return List.of();
+    @Transactional(readOnly = true)
+    public DentistDetailsDto getDentistById(Long id) {
+        dentistValidation.requireNonNull(id, "Dentist ID cannot be null");
+
+        return dentistRepository.findById(id)
+                .map(dentistMapper::toDetailedDto)
+                .orElseThrow(() -> new NotFoundException("Dentist with id: " + id + " was not found"));
     }
 
     @Override
-    public Dentist updateDentist(Long id, Dentist dentist) {
-        return null;
+    @Transactional
+    public void updateDentist(Long id, @Valid DentistDto dto) {
+        dentistValidation.requireNonNull(id, "Dentist ID cannot be null");
+
+        Dentist dentist = dentistRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Dentist with id: " + id + " was not found"));
+
+        if (!Objects.equals(dentist.getEmail(), dto.email())) {
+            dentistValidation.assertDoesNotExistByEmail(dto.email());
+        }
+
+        dentist.updateInformation(dto.firstName(), dto.lastName(), dto.email());
+        dentistRepository.save(dentist);
     }
 
     @Override
+    @Transactional
     public void deleteDentist(Long id) {
+        dentistValidation.requireNonNull(id, "Dentist ID cannot be null");
+        dentistValidation.assertExistsById(id);
 
-    }
-
-    @Override
-    public Optional<Dentist> findByEmail(String email) {
-        return dentistRepository.findByEmail(email);
+        dentistRepository.deleteById(id);
     }
 }

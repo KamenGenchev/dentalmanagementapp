afb1a09ea813d292acc87eeb6506fb77f730cf12
Kamen <genchev.kamen@gmail.com>
2024-09-16 16:00:29 +0300

    Add AdminRepository repository for Admin entity

---
+++ src/main/java/com/dentalmanagementapp/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.dentalmanagementapp.repository;
+
+import com.dentalmanagementapp.entities.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long>, IUserRepository {
+    @Override
+    Optional<Admin> findByEmail(String email);
+}

433df4f7ca6f548b9fade60a81165d81f6c3a671
Kamen <genchev.kamen@gmail.com>
2024-09-19 11:41:46 +0300

    Extend OrthodonticRecordRepository with custom repository

--- src/main/java/com/dentalmanagementapp/repository/OrthodonticRecordRepository.java
+++ src/main/java/com/dentalmanagementapp/repository/OrthodonticRecordRepository.java
@@ -1,9 +1,10 @@
 package com.dentalmanagementapp.repository;
 
 import com.dentalmanagementapp.entities.OrthodonticRecord;
+import com.dentalmanagementapp.repository.custom.CustomOrthodonticRecordRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrthodonticRecordRepository extends JpaRepository<OrthodonticRecord, Long> {
+public interface OrthodonticRecordRepository extends JpaRepository<OrthodonticRecord, Long>, CustomOrthodonticRecordRepository {
 }

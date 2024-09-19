325fc28829035dd5e6379a89fb19b37f6648f508
Kamen <genchev.kamen@gmail.com>
2024-09-19 14:32:25 +0300

    Add PatientService and endpoint to PatientController

--- src/main/java/com/dentalmanagementapp/controllers/PatientController.java
+++ src/main/java/com/dentalmanagementapp/controllers/PatientController.java
@@ -1,10 +1,25 @@
 package com.dentalmanagementapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dentalmanagementapp.dtos.PatientRecordsDto;
+import com.dentalmanagementapp.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("/patients")
+@RequestMapping("/api/patient")
 public class PatientController {
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<PatientRecordsDto> getRecords() {
+        return patientService.getAllRecordsForPatient();
+    }
 
 }

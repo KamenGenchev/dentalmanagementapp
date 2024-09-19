c70ba1319198694db61c0c08ab268cc38bdb4938
Kamen <genchev.kamen@gmail.com>
2024-09-19 14:02:35 +0300

    Remove SecurityUtil usage from DentistController

--- src/main/java/com/dentalmanagementapp/controllers/DentistController.java
+++ src/main/java/com/dentalmanagementapp/controllers/DentistController.java
@@ -5,43 +5,41 @@ import com.dentalmanagementapp.dtos.dentist.DentistDto;
 import com.dentalmanagementapp.dtos.patient.PatientAddDto;
 import com.dentalmanagementapp.service.DentistPatientService;
 import com.dentalmanagementapp.service.DentistService;
-import com.dentalmanagementapp.util.SecurityUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.dentalmanagementapp.security.UserContext.getCurrentUser;
 
 @RestController
 @RequestMapping("/api/dentists")
 public class DentistController {
     private final DentistService dentistService;
     private final DentistPatientService dentistPatientService;
-    private final SecurityUtil securityUtil;
 
     @Autowired
-    public DentistController(DentistService dentistService, DentistPatientService dentistPatientService, SecurityUtil securityUtil) {
+    public DentistController(DentistService dentistService, DentistPatientService dentistPatientService) {
         this.dentistService = dentistService;
         this.dentistPatientService = dentistPatientService;
-        this.securityUtil = securityUtil;
     }
 
     @GetMapping()
     public ResponseEntity<DentistDetailsDto> getProfile() {
-        DentistDetailsDto dentist = dentistService.getDentistById(securityUtil.getCurrentUserId());
+        DentistDetailsDto dentist = dentistService.getDentistById(getCurrentUser().currentUserId());
         return new ResponseEntity<>(dentist, HttpStatus.OK);
     }
 
     @PutMapping()
     public ResponseEntity<Void> updateSelf(@Valid @RequestBody DentistDto dentistDto) {
-        dentistService.updateDentist(securityUtil.getCurrentUserId(), dentistDto);
+        dentistService.updateDentist(getCurrentUser().currentUserId(), dentistDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/add-patient")
     public ResponseEntity<Void> addNewPatientToDentist(@Valid @RequestBody PatientAddDto patientDto) {
-        dentistPatientService.createPatientWithDentist(patientDto, securityUtil.getCurrentUserId());
+        dentistPatientService.createPatientWithDentist(patientDto, getCurrentUser().currentUserId());
         return ResponseEntity.noContent().build();
     }
 }

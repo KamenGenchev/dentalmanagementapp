ff4bc56d1dd360801b62c247f296e526f28ef5b9
Kamen <genchev.kamen@gmail.com>
2024-09-19 14:00:56 +0300

    Add CRUD operations and records retrieval to PatientService

--- src/main/java/com/dentalmanagementapp/service/PatientService.java
+++ src/main/java/com/dentalmanagementapp/service/PatientService.java
@@ -1,4 +1,19 @@
 package com.dentalmanagementapp.service;
-public interface PatientService {
 
+import com.dentalmanagementapp.dtos.patient.PatientAddDto;
+import com.dentalmanagementapp.dtos.patient.PatientDetailedDto;
+import com.dentalmanagementapp.dtos.patient.PatientDto;
+import com.dentalmanagementapp.dtos.patient.PatientUpdateDto;
+import com.dentalmanagementapp.entities.common.PatientRecord;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface PatientService {
+    Long createPatient(PatientAddDto dto);
+    PatientDetailedDto getPatientById(Long id);
+    List<PatientDto> getAllPatients();
+    void updatePatient(Long id, PatientUpdateDto patientUpdateDtoDto);
+    void deletePatient(Long id);
+    ResponseEntity<List<PatientRecord>> getAllRecordsForPatient(Long currentUserId);
 }

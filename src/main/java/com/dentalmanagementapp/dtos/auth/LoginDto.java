b05093d0569ae137161778aeca596515781edebc
Kamen <genchev.kamen@gmail.com>
2024-09-19 13:30:21 +0300

    Move LoginDto to auth package and update password validation

--- src/main/java/com/dentalmanagementapp/dtos/auth/LoginDto.java
+++ src/main/java/com/dentalmanagementapp/dtos/auth/LoginDto.java
@@ -1,4 +1,4 @@
-package com.dentalmanagementapp.dtos;
+package com.dentalmanagementapp.dtos.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Size;
 
 public record LoginDto(
         @NotEmpty @Email String email,
-        @NotEmpty @Size(min = 8, max = 25) String password
+        @NotEmpty @Size(min = 8, max = 30, message = "Password must be between 8 and 25 characters!") String password
 ) {
 }

53c23c25917761636fbffd0eba55e6f7abff49e1
Kamen <genchev.kamen@gmail.com>
2024-09-22 20:08:34 +0300

    Extend DentistPatientRepository with custom repository

--- src/main/java/com/dentalmanagementapp/repository/DentistPatientRepository.java
+++ src/main/java/com/dentalmanagementapp/repository/DentistPatientRepository.java
@@ -1,7 +1,8 @@
 package com.dentalmanagementapp.repository;
 
 import com.dentalmanagementapp.entities.DentistPatient;
+import com.dentalmanagementapp.repository.custom.CustomDentistPatientRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DentistPatientRepository extends JpaRepository<DentistPatient, Long> {
+public interface DentistPatientRepository extends JpaRepository<DentistPatient, Long>, CustomDentistPatientRepository {
 }

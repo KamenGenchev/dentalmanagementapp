1954b4d691a76913711f6c93fc3d028fb73251a9
Kamen <genchev.kamen@gmail.com>
2024-09-23 20:37:24 +0300

    Rename DentistPatientDto to PatientForDentistDto

--- src/main/java/com/dentalmanagementapp/dtos/dentistpatient/PatientForDentistDto.java
+++ src/main/java/com/dentalmanagementapp/dtos/dentistpatient/PatientForDentistDto.java
@@ -1,9 +1,10 @@
-package com.dentalmanagementapp.dtos;
+package com.dentalmanagementapp.dtos.dentistpatient;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record DentistPatientDto(
+public record PatientForDentistDto(
         @NotBlank
         @Size(min = 1, max = 50)
         String patientFirstName,
@@ -12,6 +13,7 @@ public record DentistPatientDto(
         @Size(min = 1, max = 50)
         String patientLastName,
 
+        @NotNull
         short localId
 ) {
 

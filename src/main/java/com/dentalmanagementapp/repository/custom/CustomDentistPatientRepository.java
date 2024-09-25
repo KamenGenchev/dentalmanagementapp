7b449bac39a20d56e2257ee96cfca33da0333ca8
Kamen <genchev.kamen@gmail.com>
2024-09-25 10:31:37 +0300

    Add method to find patient by local ID

--- src/main/java/com/dentalmanagementapp/repository/custom/CustomDentistPatientRepository.java
+++ src/main/java/com/dentalmanagementapp/repository/custom/CustomDentistPatientRepository.java
@@ -4,8 +4,12 @@ import com.dentalmanagementapp.entities.DentistPatient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface CustomDentistPatientRepository {
     short findNextLocalIdForDentist();
     boolean patientExistsForDentist(String email);
     Page<DentistPatient> searchPatientsByName(String firstName, String lastName, Pageable pageable);
+
+    Optional<DentistPatient> findPatientByLocalId(short localId);
 }

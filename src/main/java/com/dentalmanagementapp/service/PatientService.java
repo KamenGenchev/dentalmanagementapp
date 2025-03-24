8d20d51fa059a68173b1d06c64850b6ba24bbfb8
Kamen <genchev.kamen@gmail.com>
2025-03-24 19:50:34 +0200

    Added @Valid annotations to method parameters in PatientService for createPatient and updatePatient.

--- src/main/java/com/dentalmanagementapp/service/PatientService.java
+++ src/main/java/com/dentalmanagementapp/service/PatientService.java
@@ -5,15 +5,16 @@ import com.dentalmanagementapp.dtos.patient.PatientAddDto;
 import com.dentalmanagementapp.dtos.patient.PatientDetailedDto;
 import com.dentalmanagementapp.dtos.patient.PatientDto;
 import com.dentalmanagementapp.dtos.patient.PatientUpdateDto;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface PatientService {
-    Long createPatient(PatientAddDto dto);
+    Long createPatient(@Valid PatientAddDto dto);
     PatientDetailedDto getPatientById(Long id);
     List<PatientDto> getAllPatients();
-    void updatePatient(Long id, PatientUpdateDto patientUpdateDtoDto);
+    void updatePatient(Long id, @Valid PatientUpdateDto patientUpdateDtoDto);
     void deletePatient(Long id);
     ResponseEntity<PatientRecordsDto> getAllRecordsForPatient();
 }

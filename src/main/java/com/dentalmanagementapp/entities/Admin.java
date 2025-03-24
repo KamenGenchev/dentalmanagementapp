fe0f6255e324163cf8ff85814ffe3db4e7585258
Kamen <genchev.kamen@gmail.com>
2025-03-24 19:33:34 +0200

    Refactored Admin entity to remove unnecessary transient role field

--- src/main/java/com/dentalmanagementapp/entities/Admin.java
+++ src/main/java/com/dentalmanagementapp/entities/Admin.java
@@ -2,16 +2,11 @@ package com.dentalmanagementapp.entities;
 
 import com.dentalmanagementapp.entities.common.AbstractUser;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Transient;
-import org.springframework.beans.factory.annotation.Value;
 
 @Entity
 public class Admin extends AbstractUser {
-    @Transient
-    private final String role = "ROLE_ADMIN";
-
     @Override
     public String getRole() {
-        return role;
+        return "ROLE_ADMIN";
     }
 }

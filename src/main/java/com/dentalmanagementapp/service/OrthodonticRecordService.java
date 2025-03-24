d84ed4df5c23ad115c072820ad906e2146fdc5d5
Kamen <genchev.kamen@gmail.com>
2025-03-24 19:43:47 +0200

    Refactor OrthodonticRecordService interface
    Split service interface into two versions: one including user-specific actions with CurrentUser context, and one focused on general patient record management without CurrentUser.

--- src/main/java/com/dentalmanagementapp/service/OrthodonticRecordService.java
+++ src/main/java/com/dentalmanagementapp/service/OrthodonticRecordService.java
@@ -1,16 +1,18 @@
 package com.dentalmanagementapp.service;
 
+import com.dentalmanagementapp.dtos.record.OrthodonticRecordCreateDto;
 import com.dentalmanagementapp.dtos.record.OrthodonticRecordDto;
 import com.dentalmanagementapp.dtos.record.OrthodonticRecordUpdateDto;
-import com.dentalmanagementapp.security.CurrentUser;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 public interface OrthodonticRecordService {
-    List<OrthodonticRecordDto> getAllRecords(CurrentUser currentUser);
-    OrthodonticRecordDto getRecord(Long id, CurrentUser currentUser);
-    Long saveRecord(OrthodonticRecordDto record);
-    void updateRecord (Long id, OrthodonticRecordUpdateDto updateDto, CurrentUser currentUser);
-    void deleteRecord(Long id, CurrentUser currentUser);
+    List<OrthodonticRecordDto> getAllRecords();
+    List<OrthodonticRecordDto> getAllRecordsForPatient(short localPatientId);
+    OrthodonticRecordDto getRecord(Long id);
+    Long saveRecord(@Valid OrthodonticRecordCreateDto record);
+    void updateRecord (Long id, @Valid OrthodonticRecordUpdateDto updateDto);
+    void deleteRecord(Long id);
 
 }

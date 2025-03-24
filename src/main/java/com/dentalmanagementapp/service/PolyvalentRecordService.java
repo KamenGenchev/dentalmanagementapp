10f1a666197edef26a7813ad85917b25f6d8868a
Kamen <genchev.kamen@gmail.com>
2025-03-24 19:52:37 +0200

    Refactor PolyvalentRecordService interface for DTO usage

--- src/main/java/com/dentalmanagementapp/service/PolyvalentRecordService.java
+++ src/main/java/com/dentalmanagementapp/service/PolyvalentRecordService.java
@@ -1,14 +1,16 @@
 package com.dentalmanagementapp.service;
 
-import com.dentalmanagementapp.dtos.record.PolyvalentRecordUpdateDto;
+import com.dentalmanagementapp.dtos.PolyvalentRecordUpdateDto;
+import com.dentalmanagementapp.dtos.record.PolyvalentRecordDto;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 public interface PolyvalentRecordService {
-    List<PolyvalentRecordUpdateDto> getAllRecords();
-    PolyvalentRecordUpdateDto getRecord(Long id);
-    Long saveRecord(PolyvalentRecordUpdateDto record);
-    void updateRecord (Long id, PolyvalentRecordUpdateDto updateDto);
+    List<PolyvalentRecordDto> getAllRecords();
+    PolyvalentRecordDto getRecord(Long id);
+    Long saveRecord(@Valid PolyvalentRecordDto record);
+    void updateRecord(Long id, @Valid PolyvalentRecordUpdateDto recordUpdateDto);
     void deleteRecord(Long id);
 
 }

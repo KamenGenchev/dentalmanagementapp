57dae19977bf52dd084bafb59cade1c0481f3bd2
Kamen <genchev.kamen@gmail.com>
2025-03-24 19:34:52 +0200

    Add validation annotations to AuthService method parameters

--- src/main/java/com/dentalmanagementapp/service/AuthService.java
+++ src/main/java/com/dentalmanagementapp/service/AuthService.java
@@ -1,10 +1,11 @@
 package com.dentalmanagementapp.service;
 
-import com.dentalmanagementapp.dtos.dentist.DentistRegisterDto;
+import com.dentalmanagementapp.dtos.auth.DentistRegisterDto;
 import com.dentalmanagementapp.dtos.auth.LoginDto;
 import com.dentalmanagementapp.dtos.auth.LoginResponseDto;
+import jakarta.validation.Valid;
 
 public interface AuthService {
-    void registerDentist(DentistRegisterDto dto);
-    LoginResponseDto authenticateUser(LoginDto dto);
+    void registerDentist(@Valid DentistRegisterDto dto);
+    LoginResponseDto authenticateUser(@Valid LoginDto dto);
 }
